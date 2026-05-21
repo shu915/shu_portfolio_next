7e2ee4def4cdef8ba2905e783a71733822f39fbb
shu915 <chukichi915@gmail.com>
2026-05-21 16:48:07 +0900

    レビューを受け入れる

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -19,7 +19,7 @@ export function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${gaId}');
+          gtag('config', '${gaId}', { send_page_view: false });
         `}
       </Script>
       <Suspense fallback={null}>
